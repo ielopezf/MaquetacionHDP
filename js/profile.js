98c4a0ae4d8150a443c56b9b6308dfe9b731a665
Carlos Palomo <ceduardo.palomo@gmail.com>
2022-06-12 12:06:09 -0600

    Login funcionando

--- js/profile.js
+++ js/profile.js
@@ -6,19 +6,21 @@ if (jwt == null) {
 
 function loadUser() {
   const xhttp = new XMLHttpRequest();
-  xhttp.open("GET", "http://localhost:8081/api/usuarios/getUsuario?id=1");
+  xhttp.open("GET", "http://localhost:8081/api/usuarios/getUsuarioLogeado");
   xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
   xhttp.setRequestHeader("Authorization", jwt);
   xhttp.send();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4) {
       const objects = JSON.parse(this.responseText);
+      const userdata = objects['dataset'][0];
       console.log(objects);
       if (objects["status"] === true) {
-        const user = objects["user"]
-        document.getElementById("fname").innerHTML = datosusuario["carnet"];
-        document.getElementById("avatar").src = datosusuario["carnet"];
-        document.getElementById("username").innerHTML = datosusuario["carnet"];
+        document.getElementById("fname").innerHTML = userdata["nombre"];
+        document.getElementById("fname2").innerHTML = userdata["nombre"];
+        document.getElementById("emailrow").innerHTML = userdata["email"];
+        document.getElementById("carnetrow").innerHTML = userdata["carnet"];
+        document.getElementById("username").innerHTML = userdata["carnet"];
       }
     }
   };
